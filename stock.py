8ab0eebde81c926fc815f832d44bb8655c5cd634
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-06 03:12:49 -0600

    update

--- stock.py
+++ stock.py
@@ -42,15 +42,15 @@ print((ten_ema[ten_ema.size - 2] - ten_ema[ten_ema.size - 3]) / 15)
 print((MACD[MACD.size - 2] - MACD[MACD.size - 3]) / 15)
 
 
-fig, (ax1, ax2) = plt.subplots(2)
+# fig, (ax1, ax2) = plt.subplots(2)
 
 
-ax1.plot(df.index, ten_ema, label='FB MACD', color='blue')
-ax1.plot(df.index, ten_sma, label='Signal Line', color='red')
-ax1.legend(loc='upper left')
+# ax1.plot(df.index, ten_ema, label='FB MACD', color='blue')
+# ax1.plot(df.index, ten_sma, label='Signal Line', color='red')
+# ax1.legend(loc='upper left')
 
-ax2.plot(df.index, MACD, label='FB MACD', color='blue')
-ax2.plot(df.index, signal, label='Signal Line', color='red')
-ax2.legend(loc='upper left')
+# ax2.plot(df.index, MACD, label='FB MACD', color='blue')
+# ax2.plot(df.index, signal, label='Signal Line', color='red')
+# ax2.legend(loc='upper left')
 
-plt.show()
+# plt.show()
