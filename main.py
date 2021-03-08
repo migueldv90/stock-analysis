ce94d23e97bed890d46e8ee75f359085c048d8c1
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-08 15:18:40 -0600

    update

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     file.close()
 
 
-tickers = ['dkng', 'penn', 'flux', 'gevo', 'mu', 'jmia', 'tigr', 'nndm', 'dm', 'csiq', 'lazr', 'idex', 'nio',
-           'riot', 'mara', 'mmm', 'tan', 'icln', 'arkg', 'arkq', 'arkf', 'arkw', 'arkk', 'qqq', 'spyg', 'spy']
+tickers = ['dkng', 'penn', 'flux', 'gevo', 'mu', 'jmia', 'tigr', 'nndm', 'dm', 'csiq', 'lazr', 'idex', 'nio', 'riot', 'mara', 'mmm']
+
+tickers = ['tan', 'icln', 'arkg', 'arkq', 'arkf', 'arkw', 'arkk', 'qqq', 'spyg', 'spy']
+
+tickers = ['btc-usd', 'eth-usd']
+
+
 main()
