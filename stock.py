f7671eb51f0e85621d0c97b470a96908aeaa0c45
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-07 21:54:31 -0600

    update

--- stock.py
+++ stock.py
@@ -1,20 +1,16 @@
+from datetime import datetime, timedelta
 import yfinance as yf
-import datetime as dt
 
 
 ticker = 'btc-usd'
 
-now = dt.datetime.now()
-startyear = 2021
-startmonth = 3
-startday = 4
-start = dt.datetime(startyear, startmonth, startday)
-df = yf.download(ticker, period="2d", interval="15m")
+
+data = yf.download(ticker, period="1d", interval="1h")
 
 
-short_sma = df.Close.rolling(window=10).mean()
-short_ema = df.Close.ewm(span=10, adjust=False).mean()
-long_ema = df.Close.ewm(span=20, adjust=False).mean()
+short_sma = data.Close.rolling(window=10).mean()
+short_ema = data.Close.ewm(span=10, adjust=False).mean()
+long_ema = data.Close.ewm(span=20, adjust=False).mean()
 
 
 macd = short_ema - long_ema
