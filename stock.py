06cf42a9352a201135a95e3219d5e9863cc40ddd
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-08 00:03:35 -0600

    update

--- stock.py
+++ stock.py
@@ -1,10 +1,11 @@
+import numpy
 import yfinance
 
 
-ticker = 'btc-usd'
+ticker = 'nio'
 
 
-data = yfinance.download(ticker, period="3d", interval="1h")
+data = yfinance.download(ticker, period="5d", interval="30m", prepost=True)
 
 
 short_sma = data.Close.rolling(window=10).mean()
@@ -14,6 +15,9 @@ long_ema = data.Close.ewm(span=20, adjust=False).mean()
 
 macd = short_ema - long_ema
 signal = macd.ewm(span=15, adjust=False).mean()
+
+
+angle = numpy.rad2deg(numpy.arctan2(macd[macd.size - 1] - macd[macd.size - 2], 1))
 
 
 print('Ticker:')
@@ -27,3 +31,6 @@ print(macd[macd.size - 1])
 
 print('Velocity:')
 print(macd[macd.size - 1] - macd[macd.size - 2])
+
+print('Angle:')
+print(angle)
