9c7e4ed543f9e5e11f1e29090c99293f192918cb
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-08 00:09:52 -0600

    update

--- stock.py
+++ stock.py
@@ -16,8 +16,8 @@ long_ema = data.Close.ewm(span=20, adjust=False).mean()
 macd = short_ema - long_ema
 signal = macd.ewm(span=15, adjust=False).mean()
 
-
-angle = numpy.rad2deg(numpy.arctan2(macd[macd.size - 1] - macd[macd.size - 2], 1))
+macd_angle = numpy.rad2deg(numpy.arctan2(macd[macd.size - 1] - macd[macd.size - 2], 1))
+velocity = macd[macd.size - 1] - macd[macd.size - 2]
 
 
 print('Ticker:')
@@ -29,8 +29,8 @@ print(data.Close[data.Close.size - 1])
 print('Macd:')
 print(macd[macd.size - 1])
 
-print('Velocity:')
-print(macd[macd.size - 1] - macd[macd.size - 2])
+print('Macd Angle:')
+print(macd_angle)
 
-print('Angle:')
-print(angle)
+print('Velocity:')
+print(velocity)
