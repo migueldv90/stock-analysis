28dd4694e707376cc113c7ea1d23c1f3c492ae3d
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-08 14:21:32 -0600

    update

--- stock.py
+++ stock.py
@@ -11,7 +11,7 @@ def ticker_analysis(ticker, file):
     short_ema = data.Close.ewm(span=10, adjust=False).mean()
     long_ema = data.Close.ewm(span=20, adjust=False).mean()
 
-    short_ema_percent = ((short_ema[short_ema.size - 1] - short_ema[short_ema.size - 2]) / short_ema[short_ema.size - 1]) * 100
+    ema_10_percent = ((short_ema[short_ema.size - 1] - short_ema[short_ema.size - 2]) / short_ema[short_ema.size - 1]) * 100
 
     macd = short_ema - long_ema
 
@@ -22,7 +22,7 @@ def ticker_analysis(ticker, file):
     print(ticker, file=file)
 
     print('Ema Percent:', file=file)
-    print(short_ema_percent, file=file)
+    print(ema_10_percent, file=file)
 
     print('Macd Angle:', file=file)
     print(macd_angle, file=file)
