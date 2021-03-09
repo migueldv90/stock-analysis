3a87746875d8d0e1540f7019fd8de2ce2116348b
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-08 22:59:04 -0600

    update

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def main():
 
 tickers = ['dkng', 'penn', 'flux', 'gevo', 'mu', 'jmia', 'tigr', 'nndm', 'dm', 'csiq', 'lazr', 'idex', 'nio', 'riot', 'mara', 'mmm']
 tickers = ['tan', 'icln', 'arkg', 'arkq', 'arkf', 'arkw', 'arkk', 'qqq', 'spyg', 'spy']
-tickers = ['btc-usd', 'eth-usd']
+tickers = ['btc-usd', 'eth-usd', 'ltc-usd']
 
-time_frame = '30m'
+
+time_frame = '1d'
 
 
 main()
