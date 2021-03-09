10bd6402fcd49b867336aad6072fbced3b6df0e0
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-08 21:46:20 -0600

    update

--- stock.py
+++ stock.py
@@ -11,17 +11,17 @@ def ticker_analysis(ticker, time_frame, file):
     ema_10 = data.Close.ewm(span=10, adjust=False).mean()
     ema_20 = data.Close.ewm(span=20, adjust=False).mean()
 
-    ema_10_change = ema_10[ema_10.size - 2] - ema_10[ema_10.size - 3]
-    ema_10_change_percent = (ema_10_change / ema_10[ema_10.size - 2]) * 100
+    ema_10_delta = ema_10[ema_10.size - 2] - ema_10[ema_10.size - 3]
+    ema_10_delta_percent = (ema_10_delta / ema_10[ema_10.size - 2]) * 100
 
     macd = ema_10 - ema_20
 
     macd_angle = numpy.rad2deg(numpy.arctan2(macd[macd.size - 2] - macd[macd.size - 3], 1))
-    macd_acceleration = (macd[macd.size - 2] - macd[macd.size - 3]) - (macd[macd.size - 3] - macd[macd.size - 2])
+    macd_acceleration = (macd[macd.size - 2] - macd[macd.size - 3]) - (macd[macd.size - 3] - macd[macd.size - 4])
 
-    low = data.Low.rolling(10).min()
-    high = data.High.rolling(10).max()
-    stoch = ((data.Close[data.Close.size - 2] - low[low.size - 2]) / (high[high.size - 2] - low[low.size - 2])) * 100
+    low_10 = data.Low.rolling(10).min()
+    high_10 = data.High.rolling(10).max()
+    stoch = ((data.Close[data.Close.size - 2] - low_10[low_10.size - 2]) / (high_10[high_10.size - 2] - low_10[low_10.size - 2])) * 100
 
     status = ''
     if macd_angle > 0 and macd_acceleration > 0 and stoch > 20:
@@ -35,10 +35,10 @@ def ticker_analysis(ticker, time_frame, file):
     print(ticker + ' - ' + time_frame, file=file)
 
     print('Ema 10 Change:', file=file)
-    print(ema_10_change, file=file)
+    print(ema_10_delta, file=file)
 
     print('Ema 10 Change Percent:', file=file)
-    print(ema_10_change_percent, file=file)
+    print(ema_10_delta_percent, file=file)
 
     print('Macd Angle:', file=file)
     print(macd_angle, file=file)
