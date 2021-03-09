97aa6637fb030c7f875122866273b517c013c154
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-09 01:38:22 -0600

    update

---
+++ _scan.py
@@ -0,0 +1,21 @@
+import datetime
+import threading
+from ticker.analysis import ticker_analysis
+from tickers.robinhood import tickers
+
+
+def main():
+    file = open('tickers.txt', 'w')
+    for ticker in tickers:
+        ticker_analysis(ticker, time_period, time_interval, file)
+    file.close()
+
+    print(datetime.datetime.now().strftime('%m/%d/%y - %H:%M'))
+    threading.Timer(1800, main).start()
+
+
+time_period = '30d'
+time_interval = '1d'
+
+
+main()
