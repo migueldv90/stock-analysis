a1309a2296481d161768dcc80a992130dd90881b
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-19 10:45:23 -0500

    update

--- tickers/webull.py
+++ tickers/webull.py
@@ -1,6 +1,20 @@
 tickers = [
+    'mu',
+    'mxim',
+
+    'nio',
+    'idex',
+    'lazr',
+
+    'dkng',
+
     'dm',
+    'nndm',
+
     'csiq',
     'flux',
+    'gevo',
+
+    'riot',
     'mara',
 ]
