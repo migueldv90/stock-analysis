963a40a3cd13fd786c2e9299fd3f818d7de8a7df
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-20 00:32:12 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -1,4 +1,4 @@
-def get_status(ha_color_one, ha_color_two, macd_diff, stoch_one, stoch_diff):
+def get_status(macd_diff_one, macd_diff_two, stoch_one, stoch_diff_one):
     status = ''
 
     if stoch_one >= 80:
@@ -6,19 +6,19 @@ def get_status(ha_color_one, ha_color_two, macd_diff, stoch_one, stoch_diff):
     elif stoch_one <= 20:
         status = 'Sell - Oversold'
 
-    elif macd_diff > 0 and stoch_diff > 0 and ha_color_one == 'green' and ha_color_two == 'red':
-        status = 'Buy - Now!'
-    elif macd_diff < 0 and stoch_diff < 0 and ha_color_one == 'red' and ha_color_two == 'green':
-        status = 'Sell - Now!'
+    # elif macd_diff_one > 0 and stoch_diff > 0 and ha_color_one == 'green' and ha_color_two == 'red':
+    #     status = 'Buy - Now!'
+    # elif macd_diff_one < 0 and stoch_diff < 0 and ha_color_one == 'red' and ha_color_two == 'green':
+    #     status = 'Sell - Now!'
 
-    elif macd_diff > 0 and stoch_diff > 0 and ha_color_one == 'green':
-        status = 'Buy - Now'
-    elif macd_diff < 0 and stoch_diff < 0 and ha_color_one == 'red':
-        status = 'Sell - Now'
+    # elif macd_diff_one > 0 and stoch_diff > 0 and ha_color_one == 'green':
+    #     status = 'Buy - Now'
+    # elif macd_diff_one < 0 and stoch_diff < 0 and ha_color_one == 'red':
+    #     status = 'Sell - Now'
 
-    elif macd_diff > 0:
+    elif macd_diff_one > 0:
         status = 'Buy'
-    elif macd_diff < 0:
+    elif macd_diff_one < 0:
         status = 'Sell'
 
     return status
