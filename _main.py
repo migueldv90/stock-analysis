571ae9dba2375cce0cd487deeb1cf11bc148f8b0
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-20 00:46:59 -0500

    update

---
+++ _main.py
@@ -0,0 +1,26 @@
+import datetime
+import threading
+from ticker.analysis import analysis
+
+
+from lists.webull import tickers as wb
+
+
+def main():
+    file = open('_analysis.txt', 'w')
+    for ticker in tickers:
+        analysis(ticker, time_period, time_interval, file)
+    file.close()
+
+    print(datetime.datetime.now().strftime('%m/%d/%y - %H:%M'))
+    threading.Timer(1800, main).start()
+
+
+time_period = '30d'
+time_interval = '1d'
+
+
+tickers = wb
+
+
+main()
