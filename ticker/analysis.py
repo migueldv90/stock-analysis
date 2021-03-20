0505ba7ba8f2440c83cbdea62eb847652b7897a0
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-19 23:52:33 -0500

    update

--- ticker/analysis.py
+++ ticker/analysis.py
@@ -1,5 +1,5 @@
-import numpy
 import yfinance
+from .status import get_status
 from .stoch import get_stoch_index, get_stoch_diff
 from .macd import get_macd_data, get_macd_index, get_macd_diff
 from .heikin_ashi import get_heikin_ashi_data, get_heikin_ashi_color
@@ -21,26 +21,10 @@ def ticker_analysis(ticker, time_period, time_interval, file):
     stoch_two = get_stoch_index(data, 2)
     stoch_diff = get_stoch_diff(stoch_one, stoch_two)
 
-    ticker_status = ''
-    if stoch_one > 80:
-        ticker_status = 'Buy - Hold'
-    elif stoch_one < 20:
-        ticker_status = 'Sell - Hold'
-    elif macd_diff > 0 and stoch_diff > 0 and ha_color_one == 'green' and ha_color_two == 'red':
-        ticker_status = 'Buy - Now!'
-    elif macd_diff < 0 and stoch_diff < 0 and ha_color_one == 'red' and ha_color_two == 'green':
-        ticker_status = 'Sell - Now!'
-    elif macd_diff > 0 and stoch_diff > 0 and ha_color_one == 'green':
-        ticker_status = 'Buy - Now'
-    elif macd_diff < 0 and stoch_diff < 0 and ha_color_one == 'red':
-        ticker_status = 'Sell - Now'
-    elif macd_diff > 0:
-        ticker_status = 'Buy - Hold'
-    elif macd_diff < 0:
-        ticker_status = 'Sell - Hold'
+    status = get_status(ha_color_one, ha_color_two, macd_diff, stoch_one, stoch_diff)
 
     print('Ticker:', file=file)
-    print(ticker + ' - ' + time_interval + ' - ' + ticker_status, file=file)
+    print(ticker + ' - ' + time_interval + ' - ' + status, file=file)
 
     print('Price:', file=file)
     print(data.Close[data.Close.size - 1], file=file)
---
+++ ticker/status.py
@@ -0,0 +1,21 @@
+def get_status(ha_color_one, ha_color_two, macd_diff, stoch_one, stoch_diff):
+    status = ''
+
+    if stoch_one > 80:
+        status = 'Buy - Hold'
+    elif stoch_one < 20:
+        status = 'Sell - Hold'
+    elif macd_diff > 0 and stoch_diff > 0 and ha_color_one == 'green' and ha_color_two == 'red':
+        status = 'Buy - Now!'
+    elif macd_diff < 0 and stoch_diff < 0 and ha_color_one == 'red' and ha_color_two == 'green':
+        status = 'Sell - Now!'
+    elif macd_diff > 0 and stoch_diff > 0 and ha_color_one == 'green':
+        status = 'Buy - Now'
+    elif macd_diff < 0 and stoch_diff < 0 and ha_color_one == 'red':
+        status = 'Sell - Now'
+    elif macd_diff > 0:
+        status = 'Buy - Hold'
+    elif macd_diff < 0:
+        status = 'Sell - Hold'
+
+    return status
