3c759947ada77f17130a3c9beb7aaa349d300901
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-25 13:07:55 -0500

    update

--- _main.py
+++ _main.py
@@ -25,9 +25,9 @@ def scan():
 
 def main():
     stocks()
-    scan()
-    print(datetime.datetime.now().strftime('%m/%d/%y - %H:%M'))
-    threading.Timer(1800, main).start()
+    # scan()
+    # print(datetime.datetime.now().strftime('%m/%d/%y - %H:%M'))
+    # threading.Timer(1800, main).start()
 
 
 time_period = '200d'
--- ticker/status.py
+++ ticker/status.py
@@ -1,4 +1,4 @@
-def get_status(l_min_max, l_min, l_max, macd_diff, stoch):
+def get_status(macd_one, macd_diff, signal_one, signal_diff, stoch):
     status = ''
 
     if stoch >= 80:
@@ -6,14 +6,9 @@ def get_status(l_min_max, l_min, l_max, macd_diff, stoch):
     elif stoch <= 20:
         status = 'Sell - Oversold'
 
-    if l_min_max[-1] == l_min[-1] and macd_diff > 0:
-        status = 'Buy - Now'
-    elif l_min_max[-1] == l_max[-1] and macd_diff < 0:
-        status = 'Sell - Now'
-
-    elif macd_diff > 0:
+    elif signal_diff > 0:
         status = 'Buy'
-    elif macd_diff < 0:
+    elif signal_diff < 0:
         status = 'Sell'
 
     return status
