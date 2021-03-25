dbb0b7efb021e2cf6e53a4242e235b43e0222d7a
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-25 01:00:52 -0500

    update

--- ticker/analysis.py
+++ ticker/analysis.py
@@ -11,17 +11,15 @@ def analysis(ticker, time_period, time_interval, file):
     macd_data = get_macd_data(data)
     macd_one = get_macd_index(macd_data, 1)
     macd_two = get_macd_index(macd_data, 2)
-    macd_three = get_macd_index(macd_data, 3)
-    macd_diff_one = get_macd_diff(macd_one, macd_two)
-    macd_diff_two = get_macd_diff(macd_two, macd_three)
+    macd_diff = get_macd_diff(macd_one, macd_two)
 
     stoch_one = get_stoch_index(data, 1)
     stoch_two = get_stoch_index(data, 2)
-    stoch_diff_one = get_stoch_diff(stoch_one, stoch_two)
+    stoch_diff = get_stoch_diff(stoch_one, stoch_two)
 
     min_max, l_min, l_max = highlow(data)
 
-    status = get_status(macd_diff_one, macd_diff_two, stoch_one, stoch_diff_one)
+    status = get_status(macd_diff, stoch_one, stoch_diff)
 
     print('Ticker:', file=file)
     print(ticker + ' - ' + time_interval + ' - ' + status, file=file)
@@ -32,16 +30,13 @@ def analysis(ticker, time_period, time_interval, file):
     print('Macd:', file=file)
     print(macd_one, file=file)
 
-    print('Macd Diff One:', file=file)
-    print(macd_diff_one, file=file)
-
-    print('Macd Diff Two:', file=file)
-    print(macd_diff_two, file=file)
+    print('Macd Diff:', file=file)
+    print(macd_diff, file=file)
 
     print('Stoch:', file=file)
     print(stoch_one, file=file)
 
-    print('Stoch Diff One:', file=file)
-    print(stoch_diff_one, file=file)
+    print('Stoch Diff:', file=file)
+    print(stoch_diff, file=file)
 
     print('', file=file)
