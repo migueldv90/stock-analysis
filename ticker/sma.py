82190604bf0338da6fe9bc3fc527f6690f863f04
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-25 13:36:18 -0500

    update

--- ticker/sma.py
+++ ticker/sma.py
@@ -1,3 +1,7 @@
 def get_sma_20_data(data):
     sma_20_data = data.Close.rolling(window=20).mean()
     return sma_20_data
+
+
+def get_sma_20_index(sma_20_data, index):
+    return sma_20_data[sma_20_data.size - index]
