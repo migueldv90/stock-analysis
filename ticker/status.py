8252ce53a727fce4e53dc1eca6ab1005f8a6bf98
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-25 13:47:35 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -1,9 +1,9 @@
 def get_status(sma_20_one, sma_20_diff, macd_one, macd_diff, signal_one, signal_diff):
     status = ''
 
-    if signal_diff > 0:
+    if signal_diff > 0 and sma_20_diff > 0:
         status = 'Buy'
-    elif signal_diff < 0:
+    elif signal_diff < 0 and sma_20_diff < 0:
         status = 'Sell'
 
     return status
