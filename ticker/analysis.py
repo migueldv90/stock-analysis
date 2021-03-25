b269b7b4f86f49ca40092d80619ad459ea213236
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-24 23:41:40 -0500

    update

--- ticker/analysis.py
+++ ticker/analysis.py
@@ -1,16 +1,12 @@
 import yfinance
+from .highlow import highlow
 from .status import get_status
 from .stoch import get_stoch_index, get_stoch_diff
 from .macd import get_macd_data, get_macd_index, get_macd_diff
-from .heikin_ashi import get_heikin_ashi_data, get_heikin_ashi_color
 
 
 def analysis(ticker, time_period, time_interval, file):
     data = yfinance.download(ticker, period=time_period, interval=time_interval, prepost=True)
-
-    heikin_ashi_data = get_heikin_ashi_data(data)
-    ha_color_one = get_heikin_ashi_color(heikin_ashi_data, 1)
-    ha_color_two = get_heikin_ashi_color(heikin_ashi_data, 2)
 
     macd_data = get_macd_data(data)
     macd_one = get_macd_index(macd_data, 1)
@@ -23,6 +19,8 @@ def analysis(ticker, time_period, time_interval, file):
     stoch_two = get_stoch_index(data, 2)
     stoch_diff_one = get_stoch_diff(stoch_one, stoch_two)
 
+    min_max, l_min, l_max = highlow(data)
+
     status = get_status(macd_diff_one, macd_diff_two, stoch_one, stoch_diff_one)
 
     print('Ticker:', file=file)
@@ -30,12 +28,6 @@ def analysis(ticker, time_period, time_interval, file):
 
     print('Price:', file=file)
     print(data.Close[data.Close.size - 1], file=file)
-
-    print('Heikin Ashi One:', file=file)
-    print(ha_color_one, file=file)
-
-    print('Heikin Ashi Two:', file=file)
-    print(ha_color_two, file=file)
 
     print('Macd:', file=file)
     print(macd_one, file=file)
