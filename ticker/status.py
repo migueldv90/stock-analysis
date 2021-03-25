693dd65fd133beac510224aae6e98d2d2826d616
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-25 01:22:08 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -6,6 +6,11 @@ def get_status(l_min_max, l_min, l_max, macd_diff, stoch):
     elif stoch <= 20:
         status = 'Sell - Oversold'
 
+    elif l_min_max[-1] == l_min[-1] and macd_diff > 0:
+        status = 'Buy - Now'
+    elif l_min_max[-1] == l_max[-1] and macd_diff < 0:
+        status = 'Sell - Now'
+
     elif macd_diff > 0:
         status = 'Buy'
     elif macd_diff < 0:
