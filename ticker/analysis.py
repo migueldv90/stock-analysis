ab9ab2e3083c899f4fb345375d7b131f0f3ca100
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-25 13:42:18 -0500

    update

--- ticker/analysis.py
+++ ticker/analysis.py
@@ -1,10 +1,16 @@
 import yfinance
 from .status import get_status
+from .sma import get_sma_20_data, get_sma_20_index, get_sma_20_diff
 from .macd import get_macd_data, get_macd_index, get_macd_diff, get_signal_data, get_signal_index, get_signal_diff
 
 
 def analysis(ticker, time_period, time_interval, file):
     data = yfinance.download(ticker, period=time_period, interval=time_interval, prepost=True)
+
+    sma_20_data = get_sma_20_data(data)
+    sma_20_one = get_sma_20_index(sma_20_data, 1)
+    sma_20_two = get_sma_20_index(sma_20_data, 2)
+    sma_20_diff = get_sma_20_diff(sma_20_one, sma_20_two)
 
     macd_data = get_macd_data(data)
     macd_one = get_macd_index(macd_data, 1)
@@ -16,7 +22,7 @@ def analysis(ticker, time_period, time_interval, file):
     signal_two = get_signal_index(signal_data, 2)
     signal_diff = get_signal_diff(signal_one, signal_two)
 
-    status = get_status(macd_one, macd_diff, signal_one, signal_diff)
+    status = get_status(sma_20_one, sma_20_diff, macd_one, macd_diff, signal_one, signal_diff)
 
     print('Ticker:', file=file)
     print(ticker + ' - ' + time_interval + ' - ' + status, file=file)
--- ticker/status.py
+++ ticker/status.py
@@ -1,4 +1,4 @@
-def get_status(macd_one, macd_diff, signal_one, signal_diff):
+def get_status(sma_20_one, sma_20_diff, macd_one, macd_diff, signal_one, signal_diff):
     status = ''
 
     if signal_diff > 0:
