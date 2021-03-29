cdc2c23552fc8a56f5f465811aee17a9f0d69fe8
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-29 15:54:48 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -1,4 +1,4 @@
-def get_status(stoch_one, stoch_two, stoch_diff_one):
+def get_status(macd_one, stoch_one, stoch_two, stoch_diff):
     status = ''
 
     if stoch_one >= 80 and stoch_two >= 80:
@@ -11,14 +11,14 @@ def get_status(stoch_one, stoch_two, stoch_diff_one):
     elif stoch_one <= 20 and stoch_two > 20:
         status = 'Sell - Lower'
 
-    elif stoch_one >= 75 and stoch_diff_one > 0:
+    elif stoch_one >= 75 and stoch_diff > 0:
         status = 'Buy - Critical'
-    elif stoch_one <= 25 and stoch_diff_one < 0:
+    elif stoch_one <= 25 and stoch_diff < 0:
         status = 'Sell - Critical'
 
-    elif stoch_diff_one > 0:
+    elif stoch_diff > 0:
         status = 'Buy - Normal'
-    elif stoch_diff_one < 0:
+    elif stoch_diff < 0:
         status = 'Sell - Normal'
 
     return status
