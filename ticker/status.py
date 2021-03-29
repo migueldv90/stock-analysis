af31d73a483e5d4120be2ca8e063084ee9e353b5
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-29 16:37:55 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -1,19 +1,19 @@
 def get_status(macd_one, stoch_one, stoch_two, stoch_diff):
     status = ''
 
-    if stoch_one >= 80 and stoch_two >= 80:
+    if stoch_one >= 80 and stoch_two >= 80 and macd_one < 0:
         status = 'Buy - Overbought'
-    elif stoch_one <= 20 and stoch_two <= 20:
+    elif stoch_one <= 20 and stoch_two <= 20 and macd_one < 0:
         status = 'Sell - Oversold'
 
-    elif stoch_one >= 80 and stoch_two < 80:
+    elif stoch_one >= 80 and stoch_two < 80 and macd_one < 0:
         status = 'Buy - Upper'
-    elif stoch_one <= 20 and stoch_two > 20:
+    elif stoch_one <= 20 and stoch_two > 20 and macd_one < 0:
         status = 'Sell - Lower'
 
-    elif stoch_one >= 75 and stoch_diff > 0:
+    elif stoch_one >= 75 and stoch_diff > 0 and macd_one < 0:
         status = 'Buy - Critical'
-    elif stoch_one <= 25 and stoch_diff < 0:
+    elif stoch_one <= 25 and stoch_diff < 0 and macd_one < 0:
         status = 'Sell - Critical'
 
     elif macd_one > 0:
