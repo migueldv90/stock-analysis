20855755bb32066a25d3a873ff7778f7fec0d794
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-29 15:32:07 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -6,6 +6,11 @@ def get_status(stoch_one, stoch_two, stoch_three, stoch_diff_one, stoch_diff_two
     elif stoch_one < 20:
         status = 'Sell - Hold'
 
+    elif stoch_one >= 80 and stoch_two < 80 and stoch_diff_one > 0:
+        status = 'Buy - Upper'
+    elif stoch_one <= 20 and stoch_two > 20 and stoch_diff_one < 0:
+        status = 'Sell - Lower'
+
     elif stoch_one >= 75 and stoch_diff_one > 0:
         status = 'Buy - Critical'
     elif stoch_one <= 25 and stoch_diff_one < 0:
