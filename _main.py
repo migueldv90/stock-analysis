c51ec9505457fa26b594f937479b5807fa92ef81
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-29 16:01:05 -0500

    update

--- _main.py
+++ _main.py
@@ -1,5 +1,4 @@
 import datetime
-import threading
 from ticker.analysis import analysis
 
 
@@ -9,33 +8,37 @@ from lists.etfs import tickers as etfs
 from lists.watchlist import tickers as wl
 
 
-def stocks():
-    file = open('_stocks.txt', 'w')
+def stocks(time_period, time_interval):
+    file = open('_stocks-' + time_interval + '.txt', 'w')
     for ticker in analysis_tickers:
         analysis(ticker, time_period, time_interval, file)
     file.close()
 
 
-def scan():
-    file = open('_scan.txt', 'w')
+def scan(time_period, time_interval):
+    file = open('_scan-' + time_interval + '.txt', 'w')
     for ticker in scan_tickers:
         analysis(ticker, time_period, time_interval, file)
     file.close()
 
 
-def main():
-    stocks()
-    scan()
+def main(time_period, time_interval):
+    stocks(time_period, time_interval)
+    scan(time_period, time_interval)
     print(datetime.datetime.now().strftime('%m/%d/%y - %H:%M'))
-    threading.Timer(900, main).start()
 
 
-time_period = '200d'
-time_interval = '1d'
+time_period_30d = '30d'
+time_interval_30m = '30m'
+
+
+time_period_200d = '200d'
+time_interval_1d = '1d'
 
 
 analysis_tickers = wb
 scan_tickers = wl + cp + etfs
 
 
-main()
+main(time_period_30d, time_interval_30m)
+main(time_period_200d, time_interval_1d)
