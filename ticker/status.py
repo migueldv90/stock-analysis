001e85d5c90df8b027ed19efc1574a1e68a4d339
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-30 14:23:23 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -11,9 +11,9 @@ def get_status(macd_one, macd_two, stoch_one, stoch_two, stoch_diff):
     elif stoch_one <= 20 and stoch_two > 20 and macd_one < 0:
         status = 'Sell - Stoch - Crossover'
 
-    elif stoch_one >= 75 and stoch_diff > 0 and macd_one < 0:
+    elif stoch_one >= 55 and stoch_diff > 0 and macd_one < 0:
         status = 'Buy - Stoch - Critical'
-    elif stoch_one <= 25 and stoch_diff < 0 and macd_one < 0:
+    elif stoch_one <= 45 and stoch_diff < 0 and macd_one < 0:
         status = 'Sell - Stoch - Critical'
 
     elif macd_one > 0 and macd_two < 0:
@@ -31,9 +31,9 @@ def get_status(macd_one, macd_two, stoch_one, stoch_two, stoch_diff):
     elif stoch_one <= 20 and stoch_two > 20 and macd_one > 0:
         status = 'Sell - Macd Stoch - Crossover'
 
-    elif stoch_one >= 75 and stoch_diff > 0 and macd_one > 0:
+    elif stoch_one >= 55 and stoch_diff > 0 and macd_one > 0:
         status = 'Buy - Macd Stoch - Critical'
-    elif stoch_one <= 25 and stoch_diff < 0 and macd_one > 0:
+    elif stoch_one <= 45 and stoch_diff < 0 and macd_one > 0:
         status = 'Sell - Macd Stoch - Critical'
 
     elif macd_one > 0:
