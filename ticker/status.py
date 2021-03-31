3c698967db8fb8e5a6e2fcc598f332ff041321f6
Miguel Vazquez <migueldv90@yahoo.com>
2021-03-30 22:08:22 -0500

    update

--- ticker/status.py
+++ ticker/status.py
@@ -1,7 +1,12 @@
 def get_status(macd_one, macd_two, signal_diff, stoch_one, stoch_two, stoch_diff):
     status = ''
 
-    if stoch_one >= 80 and signal_diff > 0 and macd_one < 0:
+    if macd_one > 0 and macd_two < 0:
+        status = 'Buy - Macd - Crossover'
+    elif macd_one < 0 and macd_two > 0:
+        status = 'Sell - Macd - Crossover'
+
+    elif stoch_one >= 80 and signal_diff > 0 and macd_one < 0:
         status = 'Buy - Stoch - Overbought'
     elif stoch_one <= 20 and signal_diff < 0 and macd_one < 0:
         status = 'Sell - Stoch - Oversold'
@@ -10,11 +15,6 @@ def get_status(macd_one, macd_two, signal_diff, stoch_one, stoch_two, stoch_diff
         status = 'Buy - Stoch - Critical'
     elif stoch_one <= 40 and stoch_diff < 0 and signal_diff < 0 and macd_one < 0:
         status = 'Sell - Stoch - Critical'
-
-    elif macd_one > 0 and macd_two < 0:
-        status = 'Buy - Macd - Crossover'
-    elif macd_one < 0 and macd_two > 0:
-        status = 'Sell - Macd - Crossover'
 
     elif macd_one > 0:
         status = 'Buy - Macd - Hold'
