c382c27220f5f9a5d9059dff9c11b8164d2430e5
Miguel Vazquez <migueldv90@yahoo.com>
2021-04-06 11:25:31 -0500

    update

--- analysis/status.py
+++ analysis/status.py
@@ -1,14 +1,19 @@
 def get_status(macd_one, macd_two, signal_one, signal_two):
     status = ''
 
-    if macd_one > 0 and macd_two < 0:
+    if signal_one > 0 and signal_two < 0:
+        status = 'Buy - Now'
+    elif signal_one < 0 and signal_two > 0:
+        status = 'Sell - Now'
+
+    elif macd_one > 0 and macd_two < 0:
         status = 'Buy - Watchlist'
     elif macd_one < 0 and macd_two > 0:
         status = 'Sell - Watchlist'
 
     elif signal_one > 0:
         status = 'Buy - Hold'
-    elif signal_two < 0:
+    elif signal_one < 0:
         status = 'Sell - Hold'
 
     return status
